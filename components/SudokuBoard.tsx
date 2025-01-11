47455d8e36a660531ea87c4653e9571a80a1a496
T0nia <edtoniaelohor@gmail.com>
2025-01-11 22:28:16 +0100

    made a new component for initialboard

--- components/SudokuBoard.tsx
+++ components/SudokuBoard.tsx
@@ -4,19 +4,7 @@ import Button from './Button';
 import { solveSudoku } from '../utils/solveSudoku'; // Import solveSudoku
 import { generateRandomSudoku } from '../utils/generateSudoku'; 
 import { resetBoard } from '../utils/resetBoard';
-
-// Initial board setup (for validation)
-const initialBoard: (number | null)[][] = [
-  [5, 3, null, null, 7, null, null, null, null],
-  [6, null, null, 1, 9, 5, null, null, null],
-  [null, 9, 8, null, null, null, null, 6, null],
-  [8, null, null, null, 6, null, null, null, 3],
-  [4, null, null, 8, null, 3, null, null, 1],
-  [7, null, null, null, 2, null, null, null, 6],
-  [null, 6, null, null, null, null, 2, 8, null],
-  [null, null, null, 4, 1, 9, null, null, 5],
-  [null, null, null, null, 8, null, null, 7, 9],
-];
+import { initialBoard } from '../utils/initialBoard'; // Import initialBoard
 
 const SudokuBoard: React.FC = () => {
   const [board, setBoard] = useState(generateRandomSudoku()); 
